35cc735c3bc299587c88b1c67605ed3c4c730836
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-10-28 15:34:41 +0300

    Fix typo

--- redis-sentinel-entrypoint.sh
+++ redis-sentinel-entrypoint.sh
@@ -4,7 +4,7 @@ rm -f /etc/sentinel.conf
 
 # Launch sentinel-configurator, to create /etc/sentinel.conf
 # Repeat until it succeeds
-while [ ! -e /etc/sendinel.conf ]; do
+while [ ! -e /etc/sentinel.conf ]; do
     /sentinel-configurator.sh "$@" > /etc/sentinel.conf || rm -f /etc/sentinel.conf
     sleep 0.5
 done
