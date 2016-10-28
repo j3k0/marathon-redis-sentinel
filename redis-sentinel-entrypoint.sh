cee2c8947cb92d4fc152c1ec565fe9d6d48f9c9d
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-10-28 15:51:56 +0300

    Show content of sentinel.conf

--- redis-sentinel-entrypoint.sh
+++ redis-sentinel-entrypoint.sh
@@ -9,5 +9,9 @@ while [ ! -e /etc/sentinel.conf ]; do
     sleep 0.5
 done
 
+echo ">>>>>>>>>> FILE START: /etc/sentinel.conf"
+cat /etc/sentinel.conf
+echo "<<<<<<<<<< FILE END:   /etc/sentinel.conf"
+
 # Then launch redis-sentinel
 exec docker-entrypoint.sh redis-sentinel /etc/sentinel.conf
